1b1484ec9ee5dfd790cac571dd4c9c699fd39179
Alecto <fomenkoandrey1978@gmail.com>
2021-02-26 11:18:28 +0200

    обновление тестирования

--- hw16/main-results.js
+++ hw16/main-results.js
@@ -177,32 +177,40 @@ function ageClassification(num) {
     : null;
 }
 
-console.log('-1 :', ageClassification(-1)); // -1 : null
-console.log('5 :', ageClassification(5)); // 5 : детский возраст
-console.log('34 :', ageClassification(34)); // 34 : молодой возраст
-console.log('50 :', ageClassification(50)); // 50 : средний возраст
-console.log('65.1 :', ageClassification(65.1)); // 65.1 : пожилой возраст
-console.log('80 :', ageClassification(80)); // 80 : старческий возраст
-console.log('110 :', ageClassification(110)); // 110 : долгожители
-console.log('130 :', ageClassification(130)); // 130 : null
+
+console.log('    -1 :', ageClassification(-1)); // -1 : null
+console.log('     1 :', ageClassification(1)); // 1 : детский возраст
+console.log('    24 :', ageClassification(24)); // 24 : детский возраст
+console.log(' 24.01 :', ageClassification(24.01)); // 24.01 : молодой возраст
+console.log('    44 :', ageClassification(44)); // 44 : молодой возраст
+console.log(' 44.01 :', ageClassification(44.01)); // 44.01 : средний возраст
+console.log('    65 :', ageClassification(65)); // 65 : средний возраст
+console.log('  65.1 :', ageClassification(65.1)); // 65.1 : пожилой возраст
+console.log('    75 :', ageClassification(75)); // 75 : пожилой возраст
+console.log(' 75.01 :', ageClassification(75.01)); // 75.01 : старческий возраст
+console.log('    90 :', ageClassification(90)); // 90 : старческий возраст
+console.log(' 90.01 :', ageClassification(90.01)); // 90.01 : долгожители
+console.log('   122 :', ageClassification(122)); // 122 : долгожители
+console.log('122.01 :', ageClassification(122.01)); // 122.01 : null
+console.log('   150 :', ageClassification(150)); // 150 : null
 
 /*
- * Блок тестирования:
- * console.log(1, ageClassification(-1) === null);
- * console.log(2, ageClassification(1) === 'детский возраст');
- * console.log(3, ageClassification(24) === 'детский возраст');
- * console.log(4, ageClassification(24.01) === 'молодой возраст');
- * console.log(5, ageClassification(44) === 'молодой возраст');
- * console.log(6, ageClassification(44.01) === 'средний возраст');
- * console.log(7, ageClassification(65) === 'средний возраст');
- * console.log(8, ageClassification(65.01) === 'пожилой возраст');
- * console.log(9, ageClassification(75) === 'пожилой возраст');
- * console.log(10, ageClassification(75.01) === 'старческий возраст');
- * console.log(11, ageClassification(90) === 'старческий возраст');
- * console.log(12, ageClassification(90.01) === 'долгожители');
- * console.log(13, ageClassification(122) === 'долгожители');
- * console.log(14, ageClassification(122.01) === null);
- * console.log(15, ageClassification(150) === null);
+ * Блок тестирования, везде должны быть true:
+ * console.log('    -1 :', ageClassification(-1) === null); // -1 : null
+ * console.log('     1 :', ageClassification(1) === 'детский возраст'); // 1 : детский возраст
+ * console.log('    24 :', ageClassification(24) === 'детский возраст'); // 24 : детский возраст
+ * console.log(' 24.01 :', ageClassification(24.01) === 'молодой возраст'); // 24.01 : молодой возраст
+ * console.log('    44 :', ageClassification(44) === 'молодой возраст'); // 44 : молодой возраст
+ * console.log(' 44.01 :', ageClassification(44.01) === 'средний возраст'); // 44.01 : средний возраст
+ * console.log('    65 :', ageClassification(65) === 'средний возраст'); // 65 : средний возраст
+ * console.log('  65.1 :', ageClassification(65.1) === 'пожилой возраст'); // 65.1 : пожилой возраст
+ * console.log('    75 :', ageClassification(75) === 'пожилой возраст'); // 75 : пожилой возраст
+ * console.log(' 75.01 :', ageClassification(75.01) === 'старческий возраст'); // 75.01 : старческий возраст
+ * console.log('    90 :', ageClassification(90) === 'старческий возраст'); // 90 : старческий возраст
+ * console.log(' 90.01 :', ageClassification(90.01) === 'долгожители'); // 90.01 : долгожители
+ * console.log('   122 :', ageClassification(122) === 'долгожители'); // 122 : долгожители
+ * console.log('122.01 :', ageClassification(122.01) === null); // 122.01 : null
+ * console.log('   150 :', ageClassification(150) === null); // 150 : null
  */
 
 /*
